f196e2f2d138422c30320ebf7b9add4ec77abf44
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-06-04 23:34:57 +0900

    Update installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script installs the Uzbek Latin Hunspell dictionary on a Debian-based system.
 
-cp ./uz-lat.dic /usr/share/hunspell/uz_UZ.dic
-cp ./uz-lat.aff /usr/share/hunspell/uz_UZ.aff
+cp ./uz_Latn.dic /usr/share/hunspell/uz_UZ.dic
+cp ./uz_Latn.aff /usr/share/hunspell/uz_UZ.aff
 
